c04d0c4e4cfb46d704b902125a3ac15e03a72981
Ric Wright <rkwright@geofx.com>
2017-08-26 13:52:38 -0500

    Got it working in wireframe mode.  Not computing true elevations.  Just constructing the spheroid.

--- GeoTIFFTest/TIFFMapper.js
+++ GeoTIFFTest/TIFFMapper.js
@@ -31,46 +31,53 @@ TIFFX.TIFFMapper.prototype = {
     createGlobe: function () {
         console.log("TIFFMapper:createMesh");
 
+        var image = this.reader.image;
+        var height = image.getHeight();
+        var width = image.getWidth();
+
         this.globeGeom = new THREE.Geometry();
 
-        var vertices = this.createVertices();
+        this.createVertices( image, height, width );
 
-        this.createFaces( vertices );
+        this.createFaces( height, width );
 
         this.createMaterial();
 
         this.globeMesh = new THREE.Mesh( this.globeGeom, this.globeMat );
 
-        this.scene.add( sphere );
+        this.scene.add( this.globeMesh );
     },
 
-    createVertices: function() {
+    createVertices: function( image, height, width ) {
 
-        var image = this.reader.image;
-        var height = image.getHeight();
-        var width = image.getWidth();
         var deltaLat = 180.0 / (height-1) * Math.PI / 180.0;
         var deltaLon = 360.0 / (width-1) * Math.PI / 180.0;
         var x,y,z;
         var rasterData;
         var SCALE_FACTOR = 2;
-        var vertArray = this.create2DArray(height);
+        //var vertArray = this.create2DArray(height);
+        var max=0, min=0;
 
         var lat = 90 * Math.PI / 180.0;
         for (var i = 0; i < height; i++) {
-            var rasterWindow = [0, i, width - 1, i + 1];    // left, top, right, bottom
+            var rasterWindow = [0, i, width - 0, i + 1];    // left, top, right, bottom
             rasterData = image.readRasters({window: rasterWindow});
 
-            vertArray[i] = [];
+            //vertArray[i] = [];
 
             var lon = 0;
-            for ( var j=0; j< rasterData[0].length; j++ ) {
+            for ( var j=0; j<rasterData[0].length; j++ ) {
 
                 y = Math.sin(-lat) * SCALE_FACTOR;
                 z = Math.cos(lat) * Math.sin(-lon) * SCALE_FACTOR;
                 x = Math.cos(lat) * Math.cos(-lon) * SCALE_FACTOR;
 
-                vertArray[i][j] = new THREE.Vector3(x,y,z);
+                //vertArray[i][j] = new THREE.Vector3(x,y,z);
+
+                max = Math.max(max, rasterData[0][j]);
+                min = Math.min(min, rasterData[0][j]);
+
+                this.globeGeom.vertices.push(new THREE.Vector3(x,y,z));
 
                 lon += deltaLon;
             }
@@ -78,19 +85,28 @@ TIFFX.TIFFMapper.prototype = {
             lat -= deltaLat;
         }
 
-        return vertArray;
+        console.log("max: " + max.toFixed(1) + "  min: " + min.toFixed(1));
     },
 
-    createFaces: function ( vertices ) {
+    /**
+     * Create all the faces of the globe by striding through the vertices.
+     *
+     * @param height
+     * @param width
+     */
+    createFaces: function ( height, width ) {
         var face;
+        var vertices = this.globeGeom.vertices;
 
-        for (var i = 0; i < vertices.length - 1; i++ ) {
+        for (var i = 0; i < height - 1; i++ ) {
+            var row0 = width * i;
+            var row1 = width * (i + 1);
 
-            for (var j = 0; j < vertices[i].length - 1; j++) {
-                var v0 = vertices[i][j];
-                var v1 = vertices[i][j+1];
-                var v2 = vertices[i+1][j+1];
-                var v3 = vertices[i+1][j];
+            for (var j = 0; j < width - 1; j++) {
+                var v0 = row0 + j;
+                var v1 = row0 + j+1;
+                var v2 = row1 + j+1;
+                var v3 = row1 + j;
 
                 this.globeGeom.faces.push(new THREE.Face3(v0, v1, v2));
                 this.globeGeom.faces.push(new THREE.Face3(v0, v2, v3));
