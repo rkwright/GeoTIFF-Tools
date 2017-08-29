922696ecea6c08d888283ebb611af5b7d8ef1c0f
Ric Wright <rkwright@geofx.com>
2017-08-29 09:57:08 -0500

    Checkpoint commit before cleanup.

--- GeoTIFFTest/TIFFMapper.js
+++ GeoTIFFTest/TIFFMapper.js
@@ -50,9 +50,38 @@ TIFFX.TIFFMapper.prototype = {
 
         this.globeMesh = new THREE.Mesh( this.globeGeom, this.globeMat );
 
-        //this.globeMesh.rotation.set(Math.PI, 0, 0);
+        this.globeMesh.rotation.set(Math.PI, 0, 0);
 
         this.scene.add( this.globeMesh );
+    },
+
+    testLatLon: function() {
+
+        var lat,lon;
+        var latD,lonD;
+        for ( var j=0; j<3; j++ ) {
+            lat = -Math.PI/2 + j * Math.PI/2;
+            for (var i = 0; i < 9; i++) {
+                lon = Math.PI / 4 * i;
+                var vec = this.calcXYZFromLatLon( lat, lon, 2 );
+
+                latD = lat * 180/Math.PI;
+                lonD = lon * 180/Math.PI;
+                console.log("lat: " + latD.toFixed(1) + " lon: " + lonD.toFixed(1) +
+                    " coords: " + vec[0].toFixed(2) + ", " +vec[1].toFixed(2) + ", " +vec[2].toFixed(2));
+            }
+        }
+    },
+
+    calcXYZFromLatLon: function ( lat, lon, radius ) {
+        var phi   = Math.PI/2 - lat;
+        var theta = lon + Math.PI;
+
+        x = -(radius * Math.sin(phi) * Math.cos(theta));
+        z = (radius * Math.sin(phi) * Math.sin(theta));
+        y = (radius * Math.cos(phi));
+
+        return [x,y,z];
     },
 
     createVertices: function( image, height, width ) {
@@ -67,14 +96,17 @@ TIFFX.TIFFMapper.prototype = {
         var max=0, min=0;
         var k = 0;
 
+       // this.testLatLon();
+
         var lat = 90 * Math.PI / 180.0;
+        var vec;
         for (var i=0; i < height; i++ ) {
             var rasterWindow = [0, i, width - 0, i+1];    // left, top, right, bottom
             rasterData = image.readRasters({window: rasterWindow});
 
             var lon = 0;
-            //for ( var j=rasterData[0].length-1; j>=0; j-- ) {
-            for ( var j=0; j<rasterData[0].length; j++ ) {
+            for ( var j=rasterData[0].length-1; j>=0; j-- ) {
+            //for ( var j=0; j<rasterData[0].length; j++ ) {
 
                 // instead of using the last *real* value, use the first again so it meshes
                 if ( j === 0 )
@@ -82,15 +114,48 @@ TIFFX.TIFFMapper.prototype = {
                 else
                     k = j;
 
-                var scaleFactor = SCALE_FACTOR * (rasterData[0][k] / 1000.00 * EXAGGERATION + EARTH_DIAMETER) / EARTH_DIAMETER;
-                y = Math.sin(-lat) * scaleFactor;
-                z = Math.cos(lat) * Math.sin(-lon) * scaleFactor;
-                x = Math.cos(lat) * Math.cos(-lon) * scaleFactor;
+                // from stackoverflow
+                //x = Math.cos(lat) * Math.sin(-lon);
+                //y = Math.cos(lat) * Math.cos(-lon);
+                //z = Math.sin(-lat);
 
+                //x = Math.cos(lat) * Math.cos(-lon) * scaleFactor;
+                //y = Math.sin(-lat) * scaleFactor;
+                //z = Math.cos(lat) * Math.sin(-lon) * scaleFactor;
+
+                //x = r cos(long) sin(lat)
+                //y = r sin(long) sin(lat)
+                //z = r cos(lat)
+
+                var radius = SCALE_FACTOR * (rasterData[0][k] / 1000.00 * EXAGGERATION + EARTH_DIAMETER) / EARTH_DIAMETER;
+
+                //x = Math.sin(lon) * Math.cos(lat) * scaleFactor;
+                //y = Math.sin(lon) * Math.sin(lat) * scaleFactor;
+                //z = Math.cos(lon) * scaleFactor;
+
+                // see: http://www.mathworks.de/help/toolbox/aeroblks/llatoecefposition.html
+                //x = scaleFactor * Math.cos(lat) * Math.cos(lon);
+                //z = scaleFactor * Math.cos(lat) * Math.sin(lon);
+                //y = scaleFactor * Math.sin(lat);
+
+                // from https://stackoverflow.com/questions/28365948/javascript-latitude-longitude-to-xyz-position-on-earth-threejs
+
+                /*
+                radius = 2;
+                var vec = this.calcXYZFromLatLon( 0, 0, radius );
+                console.log("vec3: " + vec[0].toFixed(2) + ", " +vec[1].toFixed(2) + ", " +vec[2].toFixed(2));
+                vec = this.calcXYZFromLatLon( Math.PI/2, 0, radius );
+                console.log("vec3: " + vec[0].toFixed(2) + ", " +vec[1].toFixed(2) + ", " +vec[2].toFixed(2));
+                vec = this.calcXYZFromLatLon( -Math.PI/2, 0, radius );
+                console.log("vec3: " + vec[0].toFixed(2) + ", " +vec[1].toFixed(2) + ", " +vec[2].toFixed(2));
+                vec = this.calcXYZFromLatLon( 0, Math.PI, radius );
+                console.log("vec3: " + vec[0].toFixed(2) + ", " +vec[1].toFixed(2) + ", " +vec[2].toFixed(2));
+                */
                 max = Math.max(max, rasterData[0][j]);
                 min = Math.min(min, rasterData[0][j]);
 
-                this.globeGeom.vertices.push(new THREE.Vector3(x,y,z));
+                vec = this.calcXYZFromLatLon(lat, lon);
+                this.globeGeom.vertices.push(this.calcXYZFromLatLon(new THREE.Vector3(vec[0], vec[1], vec[2])));
 
                 lon += deltaLon;
             }
